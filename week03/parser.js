ce7975d5b64d4b7c99406fbb3453a31e68181540
shawnli2010 <shawnli2010@126.com>
2020-08-13 23:10:54 -0700

    implement attribute parseing logic

--- week03/parser.js
+++ week03/parser.js
@@ -1,11 +1,25 @@
+let currentToken = null;
+let currentAttribute = null;
+
+function emit(token) {
+    console.log(token);
+}
+
 const EOF = Symbol("EOF"); //EOF: End of File
 
 function data(c) {
     if (c === "<") {
         return tagOpen;
     } else if (c === EOF) {
+        emit({
+            type: "EOF"
+        });
         return ;
     } else {
+        emit({
+            type: "text",
+            content:c
+        })
         return data;
     }
 }
@@ -14,6 +28,10 @@ function tagOpen(c) {
     if (c === "/") {
         return endTagOpen;
     } else if (c.match(/^[a-zA-Z]$/)) {
+        currentToken = {
+            type: "startTag",
+            tagName: ""
+        };
         return tagName(c);
     } else {
         return ;
@@ -22,6 +40,10 @@ function tagOpen(c) {
 
 function endTagOpen(c) {
     if (c.match(/^[a-zA-Z]$/)) {
+        currentToken = {
+            type: "endTag",
+            tagName: ""
+        };
         return tagName(c);
     } else if (c === ">") {
         
@@ -38,8 +60,10 @@ function tagName(c) {
     } else if (c === "/") { // it's a selfClosingTag like <br/>, but it seems like either <br> or <br/> is considered as self-clsoing tag
         return selfClosingStartTag;
     } else if (c.match(/^[a-zA-Z]$/)) { // this character is still part of the tagName
+        currentToken.tagName += c;  //.toLowerCase();
         return tagName; // continue consuming the next tagName character
     } else if (c === ">") { // it's a start tag
+        emit(currentToken);
         return data;
     } else {
         return tagName;
@@ -49,18 +73,125 @@ function tagName(c) {
 function beforeAttributeName(c) { // we eat up all the characters except for ">"
     if (c.match(/^[\t\n\f ]$/)) {
         return beforeAttributeName;
-    } else if (c === ">") {
-        return data;
+    } else if (c === "/" || c === ">" || c === EOF) {
+        return afterAttributeName(c);
     } else if (c === "=") {
-        return beforeAttributeName;
+        
     } else {
-        return beforeAttributeName;
+        currentAttribute = {
+            name: "",
+            value: ""
+        }
+        return attributeName(c);
     }
+}
+
+function attributeName(c) {
+    if (c.match(/^[\t\n\f ]$/) || c === "/" || c == ">" || c === EOF){
+        return afterAttributeName(c);
+    } else if (c === "=") {
+        return beforeAttributeValue;
+    } else if (c === "\u0000") {
+
+    } else if (c === "\"" || c === "'" || c === "<") {
+
+    } else {
+        currentAttribute.name += c;
+        return attributeName;
+    }
+}
+
+function beforeAttributeValue(c) {
+    if (c.match(/^[\t\n\f ]$/) || c === "/" || c == ">" || c === EOF) {
+        return beforeAttributeValue; 
+    } else if (c === "\"") {
+        return doubleQuotedAttributeValue;
+    } else if (c === "\'") {
+        return singleQuotedAttributeValue;
+    } else if (c === ">") {
+
+    } else {
+        return UnquotedAttributeValue(c);
+    }
+}
+
+function doubleQuotedAttributeValue(c) {
+    if (c === "\"") {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        return afterQuotedAttributeValue;
+    } else if (c === "\u0000") {
+
+    } else if (c === EOF) {
+
+    } else {
+        currentAttribute.value += c;
+        return doubleQuotedAttributeValue;
+    }
+}
+
+function singleQuotedAttributeValue(c) {
+    if (c === "\'") {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        return afterQuotedAttributeValue;
+    } else if (c === "\u0000") {
+
+    } else if (c === EOF) {
+
+    } else {
+        currentAttribute.value += c;
+        // return doubleQuotedAttributeValue; // teacher's code, might be a mistake?
+        return singleQuotedAttributeValue;
+    }    
+}
+
+function afterQuotedAttributeValue(c) {
+    if (c.match(/^[\t\n\f ]$/)) {
+        return beforeAttributeName;
+    } else if (c === "/") {
+        return selfClosingStartTag;
+    } else if (c === ">") {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        emit(currentToken);
+        return data;
+    } else if (c === EOF) {
+
+    } else {
+        currentAttribute.value += c;
+        return doubleQuotedAttributeValue;
+    }     
+}
+
+function UnquotedAttributeValue(c) {
+    if (c.match(/^[\t\n\f ]$/) || c === "/" || c == ">" || c === EOF) {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        return beforeAttributeName;
+    } else if (c === "/") {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        return selfClosingStartTag;
+    } else if (c === ">") {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        emit(currentToken);
+        return data;
+    } else if (c === "\u0000") {
+
+    } else if (c === "\"" || c === "'" || c === "<" || c === "=" || c === "`") {
+
+    } else if (c === EOF) {
+
+    } else {
+        currentAttribute.value += c;
+        return UnquotedAttributeValue;
+    }    
+}
+
+function afterAttributeName(c) {
+
 }
 
 function selfClosingStartTag(c) {
     if (c === ">") {
         currentToken.isSelfClosing = true;
+        emit(currentToken); // reminded by comment Morty
         return data;
     } else if (c === "EOF") {
 
