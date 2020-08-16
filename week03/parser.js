6db99a63be0a26e338a8a4977c25028406178f85
shawnli2010 <shawnli2010@126.com>
2020-08-15 20:41:27 -0700

    finish week3 homework

--- week03/parser.js
+++ week03/parser.js
@@ -1,12 +1,116 @@
+const css = require("css");
+const EOF = Symbol("EOF"); //EOF: End of File
 let currentToken = null;
 let currentAttribute = null;
-
 let stack = [{type: "document", children: []}]
+let currentTextNode = null;
+let rules = [];
+
+function addCSSRules(text) {
+    var ast = css.parse(text);
+    rules.push(...ast.stylesheet.rules);
+}
+
+function match(element, selector) { // assume selector can only be id, class or tag selector
+    if (!selector || !element.attributes) {
+        return false;
+    }
+
+    if (selector.charAt(0) === "#") {
+        var attr = element.attributes.filter(attr => attr.name === 'id')[0];
+        if (attr && attr.value === selector.replace("#", '')) {
+            return true;
+        }
+    } else if (selector.charAt(0) === ".") {
+        var attr = element.attributes.filter(attr => attr.name === 'class')[0];
+        if (attr && attr.value === selector.replace(".", '')) {
+            return true;
+        }
+    } else {
+        if (element.tagName === selector) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function specificity(selector) {
+    var p = [0, 0, 0, 0];
+    var selectorParts = selector.split(" ");
+    for (var part of selectorParts) {
+      if (part.charAt(0) == "#") {
+        p[1] += 1;
+      } else if (part.charAt() == ".") {
+        p[2] += 1;
+      } else {
+        p[3] += 1;
+      }
+    }
+    return p;
+  }
+  
+  function compare(sp1, sp2) {
+    if (sp1[0] - sp2[0]) {
+      return sp1[0] - sp2[0];
+    }
+    if (sp1[1] - sp2[1]) {
+      return sp1[1] - sp2[1];
+    }
+    return sp1[3] - sp2[3];
+  }
+
+function computeCSS(element) {
+    // stack will always store the current element and all the parents of the current element
+    // but stack will be changed while we parse the html
+    // so we need to call slice() to take a snap shot of the current stack 
+    // and the reason for calling reverse is that we need to start from the current element
+    // and go from current to the most outside ancestor element(the document element)
+    var elements = stack.slice().reverse();
+    if (!element.computedStyle) {
+        element.computedStyle = {};
+    }
+
+    for (let rule of rules) {
+        var selectorParts = rule.selectors[0].split(" ").reverse();
+
+        if (!match(element, selectorParts[0])) {
+            continue;
+        }
+
+        let matched = false;
+
+        var j = 1;
+        for (var i = 0; i < elements.length; i++) {
+            if (match(elements[i], selectorParts[j])) {
+                j++;
+            }
+        }
+
+        if (j >= selectorParts.length) {
+            matched = true;
+        }
+
+        if (matched) {
+            var sp = specificity(rule.selectors[0]);
+            var computedStyle = element.computedStyle;
+            for (var declaration of rule.declarations) {
+              if (!computedStyle[declaration.property]) {
+                computedStyle[declaration.property] = {};
+              }
+              if (!computedStyle[declaration.property].specificity) {
+                computedStyle[declaration.property].value = declaration.value;
+                computedStyle[declaration.property].specificity = sp;
+              } else if (compare(computedStyle[declaration.property].specificity, sp) < 0) {
+                computedStyle[declaration.property].value = declaration.value;
+                computedStyle[declaration.property].specificity = sp;
+              }
+            }
+            console.log(element.computedStyle);
+          }
+    }
+}
 
 function emit(token) {
-    if (token.type === "text") {
-        return;
-    }
     let top = stack[stack.length - 1];
 
     if (token.type === "startTag") {
@@ -27,6 +131,8 @@ function emit(token) {
             }
         }
 
+        computeCSS(element);
+
         top.children.push(element);
         element.parent = top;
 
@@ -39,13 +145,24 @@ function emit(token) {
         if (top.tagName !== token.tagName) {
             throw new Error("Tag start end doesn't match!");
         } else {
+            if (top.tagName === "style") {
+                // Assume there is only text content in a style tag
+                addCSSRules(top.children[0].content);
+            }
             stack.pop();
         }
         currentTextNode = null;
+    } else if (token.type === "text") {
+        if (currentTextNode === null) {
+            currentTextNode = {
+                type: "text",
+                content: ""
+            }
+            top.children.push(currentTextNode);
+        }
+        currentTextNode.content += token.content;
     }
 }
-
-const EOF = Symbol("EOF"); //EOF: End of File
 
 function data(c) {
     if (c === "<") {
